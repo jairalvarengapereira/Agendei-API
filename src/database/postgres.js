a0a9037e81fd1bd1ae23177982ce1a7230aeb111
jair-ap <jairalvarengapereira@gmail.com>
2026-03-11 04:54:44 -0300

    Up 11/03/2026 04:54

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -12,6 +12,7 @@ const { Pool } = pg;
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: false,
+  family: 4, // Força IPv4
 });
 
 db.connect((err, client, release) => {
@@ -19,6 +20,7 @@ db.connect((err, client, release) => {
     console.error(`Erro ao conectar o banco de dados: ${err.message}`);
   } else {
     release();
+    console.log("Banco de dados PostgreSQL conectado com sucesso.");
   }
 });
 
