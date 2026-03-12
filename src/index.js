5300f0f343bd106bc429513df814b599e32f4867
jair-ap <jairalvarengapereira@gmail.com>
2026-03-12 18:57:05 -0300

    Up 12/03/2026 18:57

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
-
 import express from 'express';
 import cors from 'cors';
 import router from './routes.js';
 
 const app = express();
 
-app.use(express.json()); // Especifica que irá trabalhar com JSON
-app.use(cors());
+app.use(express.json());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://agendeiweb.netlify.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(router);
 
 const PORT = process.env.PORT || 3001;
