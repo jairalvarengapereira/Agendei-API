fe115eab6a4ea62809bec2b87f2352abddfe6b4a
jair-ap <jairalvarengapereira@gmail.com>
2026-04-10 08:44:54 -0300

    Corrigir CORS permitir todos os headers

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', '*']
 }));
 app.use(router);
 
