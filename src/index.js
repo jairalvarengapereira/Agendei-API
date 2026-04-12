aa161ebe856e5ae806baf2033ca3878f103a5ac0
jair-ap <jairalvarengapereira@gmail.com>
2026-04-12 09:05:05 -0300

    CORS mais permissivo com whitelist e headers manuais

--- src/index.js
+++ src/index.js
@@ -7,13 +7,39 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+const whitelist = [
+  'https://agendeiweb.netlify.app',
+  'https://agendei.netlify.app',
+  'http://localhost:5173',
+  'http://localhost:3000'
+];
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, true);
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(router);
 
 const PORT = process.env.PORT || 3001;
@@ -21,4 +47,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta: ${PORT}...`);
 });
-
